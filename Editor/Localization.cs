c9d3003441badb18b89a7cef81c8361ce8c5f22b
tliks <aoft725@gmail.com>
2024-10-28 18:55:34 +0900

    chore: not to use LocalizationManager

--- Editor/Localization.cs
+++ Editor/Localization.cs
@@ -1,11 +1,11 @@
+using System;
 using System.Collections.Generic;
-using com.aoyon.triangleselector.utils;
+using UnityEditor;
 
 namespace com.aoyon.modulecreator
 {
     public class LocalizationEditor
     {
-        
         private static Dictionary<string, string[]> _LocalizedText = new Dictionary<string, string[]>
         {
             { "Utility.ModuleCreator.description", new string[] { "Generate a Prefab containing the mesh of the selection.", "選択した箇所のメッシュを含むPrefabを生成します。" }},
@@ -25,20 +25,40 @@ namespace com.aoyon.modulecreator
             { "Utility.ModuleCreator.tooltip.IncludeIgnoreTransformsToggle", new string[] { "Output PhysBone's IgnoreTransforms", "PhysBoneのIgnoreTransformsを出力する" } },
             { "Utility.ModuleCreator.tooltip.RenameRootTransformToggle", new string[] { "Rename physbone RootTransform to ensure that the costume-side physbones are integrated. This may cause duplication.", "PhysBoneのRootTransformの名前を変更することで、衣装側のPhysBoneが確実に統合されるようにします。これにより重複が発生する可能性があります。" } },
             { "Utility.ModuleCreator.tooltip.SpecifyRootObjectLabel", new string[] { "The default root object is the parent object of the specified skinned mesh renderer object", "デフォルトのルートオブジェクトは、指定されたSkinned Mesh Rendererがついたオブジェクトの親オブジェクトです" } },
+            { "Utility.ModuleCreator.SaveName", new string[] { "Prefab Name", "Prefabの名前" } },
         };
-        
+
         private const string PreferenceKey = "com.aoyon.modulecreator.lang";
-        private static int selectedLanguageIndex = LocalizationManager.GetSelectedLanguageIndex(PreferenceKey);
+        private static string[] availableLanguages = new[] { "English", "日本語" };
+        private static int selectedLanguageIndex = GetSelectedLanguageIndex(PreferenceKey);
+
+        private static int GetSelectedLanguageIndex(string preferenceKey)
+        {
+            return Array.IndexOf(availableLanguages, EditorPrefs.GetString(preferenceKey, availableLanguages[0]));
+        }
 
         public static string GetLocalizedText(string key)
         {
-            return LocalizationManager.GetLocalizedText(_LocalizedText, key, selectedLanguageIndex);
+            if (_LocalizedText.ContainsKey(key))
+            {
+                return _LocalizedText[key][selectedLanguageIndex];
+            }
+            return $"[Missing: {key}]";
         }
 
         public static void RenderLocalize()
         {
-            LocalizationManager.RenderLocalize(ref selectedLanguageIndex, PreferenceKey);
+            int languageIndex = EditorGUILayout.Popup("Language", selectedLanguageIndex, availableLanguages);
+            if (languageIndex != selectedLanguageIndex)
+            {
+                selectedLanguageIndex = languageIndex;
+                SetLanguage(languageIndex, PreferenceKey);
+            }
         }
 
+        private static void SetLanguage(int languageIndex, string preferenceKey)
+        {
+            EditorPrefs.SetString(preferenceKey, availableLanguages[languageIndex]);
+        }
     }
 }
